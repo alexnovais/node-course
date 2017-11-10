71c7ed4dc28a3f6183bc199c2f74252fe49f76e1
Alex Novais <alexnovais@gmail.com>
2017-11-10 15:46:55 -0200

    continuanto o curso

--- web-server/server.js
+++ web-server/server.js
@@ -1,8 +1,17 @@
 var express = require('express');
 var app = express();
+var PORT = 3000;
 
-app.get('/', function(req, res) {
-    res.send('Hello Express')
+var middleware = require('./middleware.js');
+
+app.use(middleware.logger);
+
+app.get('/about', middleware.requireAuthentication, function (req, res) {
+    res.send("About Us")
 });
 
-app.listen(3000);
+app.use(express.static(__dirname + '/public'));
+
+app.listen(PORT, function () {
+    console.log('Express server started on port ' + PORT + '!');
+});
